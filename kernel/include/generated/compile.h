bf5bb534fa5c2139ad40f7d8a0d1b7ef3d6e9cbf
xdavidwu <david20000612@gmail.com>
2015-05-03 10:31:03 +0800

    Fix frandom cherry-pick

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 53 */
+/* This file is auto generated, version 54 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#53 SMP PREEMPT Sat May 2 17:42:20 CST 2015"
+#define UTS_VERSION "#54 SMP PREEMPT Sun May 3 09:43:19 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
