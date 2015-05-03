586f31d2e4a94810227feeae100807c28e4ec416
xdavidwu <david20000612@gmail.com>
2015-05-03 14:43:12 +0800

    Clean file in wrong dir

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 54 */
+/* This file is auto generated, version 55 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#54 SMP PREEMPT Sun May 3 09:43:19 CST 2015"
+#define UTS_VERSION "#55 SMP PREEMPT Sun May 3 12:47:13 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A9) "
