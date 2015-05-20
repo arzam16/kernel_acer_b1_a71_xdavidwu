260661fa4fb721a8c8c47c58e0403928c79cedf3
xdavidwu <david20000612@gmail.com>
2015-05-20 22:03:22 +0800

    Fix the MC3210 accelerometer

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 55 */
+/* This file is auto generated, version 61 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#55 SMP PREEMPT Sun May 3 12:47:13 CST 2015"
+#define UTS_VERSION "#61 SMP PREEMPT Wed May 20 21:52:40 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
-#define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A9) "
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
