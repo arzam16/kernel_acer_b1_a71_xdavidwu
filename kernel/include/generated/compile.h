daeb6cb26e2ee5c2b7aaf27da4b3ede6b9af3804
xdavidwu <david20000612@gmail.com>
2015-05-22 21:31:07 +0800

    Add BFQ & SIO, remove compiled binary, update .gitignore

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 61 */
+/* This file is auto generated, version 65 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#61 SMP PREEMPT Wed May 20 21:52:40 CST 2015"
+#define UTS_VERSION "#65 SMP PREEMPT Fri May 22 21:24:04 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
