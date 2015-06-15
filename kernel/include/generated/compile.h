c2de8497ab97f4e4aaa0d43d90ec52faaa31c0b2
xdavidwu <david20000612@gmail.com>
2015-06-15 17:48:09 +0800

    Linux 3.4.26->27

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 95 */
+/* This file is auto generated, version 96 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#95 SMP PREEMPT Mon Jun 15 17:43:00 CST 2015"
+#define UTS_VERSION "#96 SMP PREEMPT Mon Jun 15 17:46:18 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- kernel/include/linux/version.h
+++ kernel/include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 197658
+#define LINUX_VERSION_CODE 197659
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
