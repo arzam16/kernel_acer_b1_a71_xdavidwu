6de39336858680b4565a89dd36866bcfb60d95e6
xdavidwu <david20000612@gmail.com>
2015-06-15 11:40:34 +0800

    GCC flag changed to -Ofast

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 67 */
+/* This file is auto generated, version 72 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#67 SMP PREEMPT Sun May 24 14:59:35 CST 2015"
+#define UTS_VERSION "#72 SMP PREEMPT Mon Jun 15 11:34:16 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
