f6b4ab8d4f0ed750121d60290d82a9a621a4b7fc
xdavidwu <david20000612@gmail.com>
2015-06-16 09:32:46 +0800

    Liux 3.4.40->41

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 99 */
+/* This file is auto generated, version 100 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#99 SMP PREEMPT Mon Jun 15 18:30:07 CST 2015"
+#define UTS_VERSION "#100 SMP PREEMPT Mon Jun 15 18:42:15 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
