c69a6d9265525520339c67257c688a545ca17bd8
xdavidwu <david20000612@gmail.com>
2015-06-16 09:42:41 +0800

    Linux 3.4.45->46

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 100 */
+/* This file is auto generated, version 101 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#100 SMP PREEMPT Mon Jun 15 18:42:15 CST 2015"
+#define UTS_VERSION "#101 SMP PREEMPT Tue Jun 16 09:39:43 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
