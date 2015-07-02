1deed14a20ada23289cb62cac1354b37c5b5c72f
xdavidwu <david20000612@gmail.com>
2015-07-02 16:09:40 +0800

    Linux 3.4.91->92 Not applied: kernel/include/linux/mm_types.h

--- kernel/include/config/auto.conf.cmd
+++ kernel/include/config/auto.conf.cmd
@@ -775,7 +775,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "3.4.90"
+ifneq "$(KERNELVERSION)" "3.4.92"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm"
