bc8cc6ecfe854d3f5fde9cc508bd7eb1b30fae9d
xdavidwu <david20000612@gmail.com>
2015-07-02 16:20:51 +0800

    Linux 3.4.92->93

--- kernel/include/config/auto.conf.cmd
+++ kernel/include/config/auto.conf.cmd
@@ -775,7 +775,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "3.4.92"
+ifneq "$(KERNELVERSION)" "3.4.93"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm"
--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 142 */
+/* This file is auto generated, version 143 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#142 SMP PREEMPT Thu Jul 2 16:05:58 CST 2015"
+#define UTS_VERSION "#143 SMP PREEMPT Thu Jul 2 16:18:08 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
