f7fac85fac387015b999dac26c56fe04c1d8ec98
xdavidwu <david20000612@gmail.com>
2015-07-02 17:17:04 +0800

    clean files

--- kernel/include/generated/autoconf.h
+++ kernel/include/generated/autoconf.h
@@ -1,7 +1,7 @@
 /*
  *
  * Automatically generated file; DO NOT EDIT.
- * Linux/arm 3.4.100 Kernel Configuration
+ * Linux/arm 3.4.104 Kernel Configuration
  *
  */
 #define CONFIG_MTK_USB_GADGET 1
--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 145 */
+/* This file is auto generated, version 147 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#145 SMP PREEMPT Thu Jul 2 16:40:59 CST 2015"
+#define UTS_VERSION "#147 SMP PREEMPT Thu Jul 2 17:05:40 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
