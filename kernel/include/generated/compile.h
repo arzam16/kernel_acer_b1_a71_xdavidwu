46c38d0fb59f3938ce8ad9ded898b5d9e001385e
xdavidwu <david20000612@gmail.com>
2015-07-09 10:17:27 +0800

    zcache from Linux 3.8

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 161 */
+/* This file is auto generated, version 175 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#161 SMP PREEMPT Fri Jul 3 14:35:59 CST 2015"
+#define UTS_VERSION "#175 SMP PREEMPT Thu Jul 9 10:05:38 CST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "xdavidwu-System-Product-Name"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
