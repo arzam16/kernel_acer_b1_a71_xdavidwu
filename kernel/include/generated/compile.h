689e3b109a88d3e56feee126f857aa053d163521
arzamas-16 <arzamas-16@hotmail.com>
2020-07-25 19:04:54 +0300

    make clean && make autoclean

--- kernel/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 7 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Thu Dec 3 20:23:50 CST 2015"
-#define LINUX_COMPILE_BY "xdavidwu"
-#define LINUX_COMPILE_HOST "xdavidwu-Xubuntu"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- kernel/include/generated/utsrelease.h
+++
@@ -1,3 +0,0 @@
-#define UTS_RELEASE "3.4.110-xdavidwu"
-#define BUILD_INFO "ALPS.JB.MP.V1.15"
-#define BUILD_FINGERPRINT "acer17_tb_wifi_only_jb"
